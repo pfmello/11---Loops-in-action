1766e935a621c79a3ed3452edbce481c6a547f5b
PFMello <philipmikh@gmail.com>
2023-05-05 02:25:30 -0300

    third section done

--- loops-in-action.js
+++ loops-in-action.js
@@ -15,3 +15,39 @@ function calculateSum() {
   outputResultElement.textContent = sum;
   outputResultElement.style.display = "block";
 }
+
+//Second example: for-of loops
+const highlightLinksButton = document.querySelector("#highlight-links button");
+highlightLinksButton.addEventListener("click", highlightAllAnchorElements);
+
+function highlightAllAnchorElements() {
+  const anchorElements = document.querySelectorAll("#highlight-links a");
+
+  for (const anchor of anchorElements) {
+    anchor.classList.add("highlight");
+  }
+}
+
+//Third Example for-in
+
+const userData = {
+  firstName: "Bigode",
+  lastName: "Bafonso",
+  age: 29,
+  sex: "gay",
+  job: "vagabundo",
+};
+
+const displayUserDataButton = document.querySelector("#user-data button");
+displayUserDataButton.addEventListener("click", displayUserData);
+
+function displayUserData() {
+  const outputListElement = document.querySelector("#user-data ul");
+  console.log(outputListElement);
+
+  for (const key in userData) {
+    const newLi = document.createElement("li");
+    newLi.textContent = key + ": " + userData[key];
+    outputListElement.append(newLi);
+  }
+}
